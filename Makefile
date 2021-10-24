2922ba2b1f815cbe08340cae56a27d0dbba1ed15
Mark Cody <markcody@Marks-MacBook-Pro.local>
2021-10-23 21:44:44 -0500

    Correcting test and flask import

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ install:
 	pip install -r requirements.txt
 
 test: 
-	python -m pytest -vv --cov=MSDS434-W5 tests/*.py
+	python -m pytest -vv --cov=MSDS434-W5 main_test.py
 
 lint:
 	pylint --disable=R,C lib 
--- main.py
+++ main.py
@@ -10,5 +10,23 @@ def hello_world():
     return greeting
 
 
+# Instantiate a bigquery client.
+query_client = bigquery.Client()
+
+# Describe the query.
+query = """
+      SELECT
+      unique_session_id,
+      predicted_will_buy_on_return_visit_probs
+      FROM `msds434-w5.ecommerce.Predictions`
+      LIMIT 100
+    """
+
+# Ask for a query job
+query_job = query_client.query(query)
+print("The prediction:")
+print(type(query_job))
+print(query_job)
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
